5d7e5b95e581d6ae477975a9d7718ccb4106f406
klok <Kamil.Owczarek@gft.com>
2018-11-02 15:57:00 +0100

    Task 1 without solutions

--- src/main/scala/prv/saevel/spark/sql/workshop/io/SparkSqlIo.scala
+++ src/main/scala/prv/saevel/spark/sql/workshop/io/SparkSqlIo.scala
@@ -4,19 +4,9 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
 
 object SparkSqlIo {
 
-  def readJson(file: String)(implicit sqlContext: SQLContext): DataFrame = {
-    sqlContext.read.json(file)
-  }
+  def readJson(file: String)(implicit sqlContext: SQLContext): DataFrame = ???
 
-  def readCsv(file: String)(implicit sqlContext: SQLContext): DataFrame = {
-    import sqlContext.implicits._
-    sqlContext.sparkContext
-      .textFile(file)
-      .map(_.split(","))
-      .map(s => Survey(s(0).toLong, s(1), s(2), s(3).toInt)).toDF
-  }
+  def readCsv(file: String)(implicit sqlContext: SQLContext): DataFrame = ???
 
-  def readOrc(file: String)(implicit sqlContext: SQLContext): DataFrame = {
-    sqlContext.read.orc(file)
-  }
+  def readOrc(file: String)(implicit sqlContext: SQLContext): DataFrame = ???
 }
