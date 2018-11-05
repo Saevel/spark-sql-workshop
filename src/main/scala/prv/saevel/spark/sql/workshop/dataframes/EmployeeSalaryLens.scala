87d822300a5b050324672fac7f950e7efda55609
klok <Kamil.Owczarek@gft.com>
2018-11-05 13:26:31 +0100

    Task 2

--- src/main/scala/prv/saevel/spark/sql/workshop/dataframes/EmployeeSalaryLens.scala
+++ src/main/scala/prv/saevel/spark/sql/workshop/dataframes/EmployeeSalaryLens.scala
@@ -1,9 +1,10 @@
 package prv.saevel.spark.sql.workshop.dataframes
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SQLContext}
 
 object EmployeeSalaryLens {
 
   def apply(department: String, minSalary: Double, maxSalary: Double)
-           (employees: DataFrame, assignments: DataFrame, salaries: DataFrame): DataFrame = ???
+           (employees: DataFrame, assignments: DataFrame, salaries: DataFrame)
+           (implicit sqlContext: SQLContext): DataFrame = ???
 }
